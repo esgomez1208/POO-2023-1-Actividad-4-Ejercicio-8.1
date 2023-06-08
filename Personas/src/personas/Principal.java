c0ed75da23d4bd197fc77ed7a7a9a7f70f86bcd6
Esteban Gómez Benítez <69543140+esgomez1208@users.noreply.github.com>
2023-06-08 01:39:16 -0500

    Ingreso de Archivos
    Codigo fuente, Diagrama de clases y PDF final.

---
+++ Personas/src/personas/Principal.java
@@ -0,0 +1,10 @@
+package personas;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
